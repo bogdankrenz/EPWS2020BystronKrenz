f4e44e9e2b4b5d8493690648acd5edc86be98b17
Carlos <carlosbystron@yahoo.de>
2020-12-28 12:21:40 +0100

    Added MongoDB

--- Main_Prototype/server/src/index.js
+++ Main_Prototype/server/src/index.js
@@ -1,8 +1,16 @@
+if (process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
+
 const express = require("express")
 const app = express()
-
-const db = require('./persistence')
 const indexRouter = require("./routes/index")
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,  {useNewUrlParser: true,  useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use("/", indexRouter)
 
