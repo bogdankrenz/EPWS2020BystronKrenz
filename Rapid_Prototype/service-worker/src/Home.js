8ef757efbcf4a8062792659d5efb67e3050c0e21
Bogdan Krenz (inf852) <bogdan.krenz@smail.th-koeln.de>
2021-01-06 16:33:50 +0100

    tried to add safari support

--- Rapid_Prototype/service-worker/src/Home.js
+++ Rapid_Prototype/service-worker/src/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
-export default function Home(){
-    return (
-        <div>
-            <h1>Home Component</h1>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Component</h1>
+
+      <Button variant="success">Sign in with Spotify</Button>
+    </div>
+  );
 }
