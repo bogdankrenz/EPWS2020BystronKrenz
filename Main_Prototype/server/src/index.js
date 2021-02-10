41338dd955b2000d6fdbf1ca293518c1bb1ea84b
Carlos <carlosbystron@yahoo.de>
2021-02-10 22:56:47 +0100

    Rechanged to localhost

--- Main_Prototype/server/src/index.js
+++ Main_Prototype/server/src/index.js
@@ -33,7 +33,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-      origin: "https://party-together-server.herokuapp.com",
+      origin: "http://localhost:3000",
       credentials: true
     }
 })
