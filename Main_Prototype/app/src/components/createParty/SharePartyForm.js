ea12919feae5daca068c1469b541867871e2802a
Bogdan Krenz (inf852) <bogdan.krenz@smail.th-koeln.de>
2021-02-18 14:28:28 +0100

    added copy to clipboard button

--- Main_Prototype/app/src/components/createParty/SharePartyForm.js
+++ Main_Prototype/app/src/components/createParty/SharePartyForm.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { FRONTEND_URL } from "../../helpers/constants";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import IconButton from "@material-ui/core/IconButton";
+import QRCode from "react-qr-code";
 
 import {
   EmailShareButton,
@@ -33,14 +36,24 @@ const useStyles = makeStyles((theme) => ({
   heading: {
     textAlign: "center",
   },
+  copy: {},
 }));
 
 export default function SharePartyForm(params) {
+  const [copy, setCopy] = useState("");
+
   const classes = useStyles();
 
   const partyURL = `${FRONTEND_URL}/join/${params.partyID}`;
   const subject = `Join ${params.partyName} on PartyTogether to make the music yours!`;
+  const textAreaRef = useRef(partyURL);
 
+  const copyToClipboard = (e) => {
+    textAreaRef.current.select();
+    document.execCommand("copy");
+    e.target.focus();
+    setCopy("Copied!");
+  };
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom className={classes.heading}>
@@ -91,6 +104,19 @@ export default function SharePartyForm(params) {
           >
             <EmailIcon size={32} round />
           </EmailShareButton>
+
+          <IconButton
+            aria-label="delete"
+            url={partyURL}
+            className={(classes.copy, classes.social)}
+            onClick={() => {
+              navigator.clipboard.writeText(partyURL);
+              setCopy("Copied!");
+            }}
+          >
+            <FileCopyIcon />
+          </IconButton>
+          {copy}
         </div>
       </Grid>
     </React.Fragment>
