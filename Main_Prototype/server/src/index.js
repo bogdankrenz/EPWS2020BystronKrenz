a38878b265b60645507002d0aaa73ad380839d42
Carlos <carlosbystron@yahoo.de>
2021-02-20 16:20:01 +0100

    Fixed typo in url

--- Main_Prototype/server/src/index.js
+++ Main_Prototype/server/src/index.js
@@ -35,7 +35,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-      origin: "https://party-together.herokuapp.com/",
+      origin: "https://party-together.herokuapp.com",
       credentials: true
     }
 })
