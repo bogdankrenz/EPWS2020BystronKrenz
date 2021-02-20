03dcd00ff4201253c0ab2adccc6d65c66e7636e1
Carlos <carlosbystron@yahoo.de>
2021-02-20 16:05:16 +0100

    changed socket url in server

--- Main_Prototype/server/src/index.js
+++ Main_Prototype/server/src/index.js
@@ -35,7 +35,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://party-together.herokuapp.com/",
       credentials: true
     }
 })
